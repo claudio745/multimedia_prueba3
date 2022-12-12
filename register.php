12ac47a3ccd3522cc1103c65bf127707860aa998
JMezaV2020 <josemeza@alumnos.upla.cl>
2022-12-12 01:14:25 -0300

    s

--- register.php
+++ register.php
@@ -26,7 +26,7 @@
                 <a class="navbar-brand" href="#">
                     <img src="img/LogorecortedWhite.png" alt="" width="50" height="35">
                 </a>
-                <a class="navbar-brand" href="#"><FONT COLOR="#FFFFFF">INICIO</FONT></a>
+                <a class="navbar-brand" href="index.php"><FONT COLOR="#FFFFFF">INICIO</FONT></a>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
                         <a class="nav-link" href="login.php"><FONT COLOR="#FFFFFF">Iniciar sesión</FONT></a>
@@ -44,13 +44,13 @@
                     <div class="col mt-2 mb-2 d-flex justify-content-center">
                         <form action="registrarCodigo.php" method="POST">
                             <label"><FONT COLOR="#FFFFFF"> Nombres: </FONT></label>
-                            <input type="text" class="form-control" name="Nombres" id="Nombres"/>
+                            <input type="text" class="form-control" name="Nombres" id="Nombres" required/>
                             <br><br>
                             <label"><FONT COLOR="#FFFFFF"> Apellido Paterno: </FONT></label>
-                            <input type="text" class="form-control" name="Apellido1" id="Apellido1"/>
+                            <input type="text" class="form-control" name="Apellido1" id="Apellido1" required/>
                             <br><br>
                             <label"><FONT COLOR="#FFFFFF"> Apellido Materno: </FONT></label>
-                            <input type="text" class="form-control" name="Apellido2" id="Apellido2"/>
+                            <input type="text" class="form-control" name="Apellido2" id="Apellido2" required/>
                             <br><br>
                             <label"><FONT COLOR="#FFFFFF"> Genero: </FONT></label>
                             <select name="Genero" class="form-control" id="Genero">
@@ -59,19 +59,19 @@
                                 <option value="Otro">Otro</option>
                             </select>                                <br><br>
                             <label"><FONT COLOR="#FFFFFF"> Fecha de nacimiento: </FONT></label>
-                            <input type="date" class="form-control" name="Fecha" id="Fecha" min="1900-01-01" max="2016-01-01"> 
+                            <input type="date" class="form-control" name="Fecha" id="Fecha" min="1900-01-01" max="2016-01-01" required> 
                             <br><br>
                             <label"><FONT COLOR="#FFFFFF"> Ciudad de residencia: </FONT></label>
-                            <input type="text" class="form-control" name="Ciudad" id="Ciudad"/>
+                            <input type="text" class="form-control" name="Ciudad" id="Ciudad" required/>
                             <br><br>
                             <label"><FONT COLOR="#FFFFFF"> Correo </FONT></label>
-                            <input type="email" class="form-control" name="Correo" id="Correo"/>
+                            <input type="email" class="form-control" name="Correo" id="Correo" required/>
                             <br><br>
                             <label"><FONT COLOR="#FFFFFF"> Rut: </FONT></label>
-                            <input type="text" class="form-control" maxlength="11" name="Rut" id="Rut" placeholder="12345678-k"/>
+                            <input type="text" class="form-control" maxlength="11" name="Rut" id="Rut" placeholder="12345678-k" pattern="[0-9]+[-][0-9-k]" required/>
                             <br><br>
                             <label for="Contraseña"><FONT COLOR="#FFFFFF"> Contraseña: </FONT></label>
-                            <input type="password" class="form-control" name="Contraseña" id="Contraseña">
+                            <input type="password" class="form-control" name="Contraseña" id="Contraseña" required>
                             <br>
                             <button type="submit" class="btn btn-secondary">Registrarse</button>
                             <a href = "login.php"><button type="button" class="btn btn-secondary">Volver</button></a>
--- registrarCodigo.php
+++ registrarCodigo.php
@@ -1,52 +1,24 @@
 <?php
-include 'log.php';
 require_once('conexionCodigo.php');
-
-$contrasenas_no_seguras = array("123456", "password", "123456789", "guest", "qwerty", "12345678", "11111", "12345", "col123456", "123123", "1234", "1234567890", "000000", "555555", "666666", "654321", "123321", "777777", "123");
 
 if (empty($_POST['Nombres']) || empty($_POST['Apellido1']) || empty($_POST['Apellido2']) || empty($_POST['Ciudad']) || empty($_POST['Correo']) || empty($_POST['Rut']) || empty($_POST['Contraseña'])){
     header("Location: http://127.0.0.1/multimedia_prueba3/register.php");
 }
 else{
-    $Nombres=$_POST['Nombres'];
-    $Apellido1=$_POST['Apellido1'];
-    $Apellido2=$_POST['Apellido2'];
-    $Genero=$_POST['Genero'];
-    $Fecha=$_POST['Fecha'];
-    $Ciudad=$_POST['Ciudad'];
-    $Correo=$_POST['Correo'];
-    $Rut=$_POST['Rut'];
-    $Contraseña=$_POST['Contraseña'];
+  $Nombres=$_POST['Nombres'];
+  $Apellido1=$_POST['Apellido1'];
+  $Apellido2=$_POST['Apellido2'];
+  $Genero=$_POST['Genero'];
+  $Fecha=$_POST['Fecha'];
+  $Ciudad=$_POST['Ciudad'];
+  $Correo=$_POST['Correo'];
+  $Rut=$_POST['Rut'];
+  $Contraseña=$_POST['Contraseña'];
 
-    /*if ((strlen($Contraseña) < 8) && (!preg_match("/[a-z]/", $Contraseña) && !preg_match("/[0-9]/", $Contraseña)) && (in_array($Contraseña, $contrasenas_no_seguras))) {
-        echo '<script>alert("Contraseña insegura, intente de nuevo")</script>';
-    }
-     if(empty($_POST['Rut'])){
-            // separar el RUT en número y dígito verificador
-            $partes = explode("-", $Rut);
-            $numero = $partes[0];
-            $digito = $partes[1];
-          
-            // aplicar la fórmula de verificación de RUT
-            $multiplos = array(2, 3, 4, 5, 6, 7);
-            $total = 0;
-            for ($i = strlen($numero) - 1; $i >= 0; $i--) {
-              $total += $numero[$i] * $multiplos[$i % 6];
-            }
-            $resto = $total % 11;
-            $digitoVerificador = ($resto == 10) ? "K" : $resto;
-          
-            // comparar el dígito verificador calculado con el dígito verificador del RUT
-            if ($digitoVerificador == $digito) {
-              return true;
-            }
-    } */
-    else{
-        $pass = password_hash($Contraseña, PASSWORD_DEFAULT);
-        $query = "INSERT INTO usuario (nombres, apellido1, apellido2, genero, fechaNacim, ciudadResid, correo, rut, contraseña)
-        VALUES ('$Nombres', '$Apellido1', '$Apellido2', '$Genero', '$Fecha', '$Ciudad', '$Correo', '$Rut', '$pass')";
+  $pass = password_hash($Contraseña, PASSWORD_DEFAULT);
+  $query = "INSERT INTO usuario (nombres, apellido1, apellido2, genero, fechaNacim, ciudadResid, correo, rut, contraseña)
+  VALUES ('$Nombres', '$Apellido1', '$Apellido2', '$Genero', '$Fecha', '$Ciudad', '$Correo', '$Rut', '$pass')";
 
-        
-        header("Location: http://127.0.0.1/multimedia_prueba3/login.php");
-    }
+  header("Location: http://127.0.0.1/multimedia_prueba3/login.php");
 }
+?>
