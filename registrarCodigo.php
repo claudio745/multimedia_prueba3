34c51d19cedc1e4978699c1d549d29937c90db3e
JMezaV2020 <josemeza@alumnos.upla.cl>
2022-12-12 01:22:06 -0300

    Update registrarCodigo.php

--- registrarCodigo.php
+++ registrarCodigo.php
@@ -18,6 +18,7 @@ else{
   $pass = password_hash($Contraseña, PASSWORD_DEFAULT);
   $query = "INSERT INTO usuario (nombres, apellido1, apellido2, genero, fechaNacim, ciudadResid, correo, rut, contraseña)
   VALUES ('$Nombres', '$Apellido1', '$Apellido2', '$Genero', '$Fecha', '$Ciudad', '$Correo', '$Rut', '$pass')";
+  $resultado = $conexion->query($query);
 
   header("Location: http://127.0.0.1/multimedia_prueba3/login.php");
 }
